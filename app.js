6c388cb2ed9daefee2ed112ad0b863ddd50d6cdd
Boaz Kaufman <boaz.kaufman97@myhunter.cuny.edu>
2022-03-22 14:17:23 -0400

    require express.json() to parse body

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json()) // for parsing application/json
+
 app.get('/', (req, res) => {
   res.send('This is the server for B-sharp!')
   console.log('Got a GET request\n');
@@ -9,7 +11,13 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
   console.log('Got a POST request');
-  console.log(req.body)
+  if (req.body){
+    console.log(req.body);
+  }
+  else {
+    console.log("No body knows");
+  }
+
 })
 
 app.listen(port, () => {
