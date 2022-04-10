a9578d936c3368b3c8e28dccfd76d2c22d3fd818
Boaz Kaufman <boaz.kaufman97@myhunter.cuny.edu>
2022-04-10 12:58:21 -0400

    destringify blob

--- app.js
+++ app.js
@@ -3,16 +3,20 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
+// Middleware to parse the req body as text (it's stringified JSON)
 app.use(bodyParser.text());
 
+/* Ignore GET requests!
 app.get('/', (req, res) => {
   res.send(recentimage)
   console.log('Got a GET request\n');
-})
+})*/
 
+// When receiving a POST, log its contents to confirm proper receipt
 app.post('/', (req, res) => {
   console.log('Got a POST request ' + Date.now());
-  console.log(JSON.parse(req.body));
+  pic = JSON.parse(JSON.parse(req.body));
+  console.log(pic);
   res.send('ok');
 })
 
