3534c5d75072b759353c954b58f0e48dd1a2efdc
Boaz Kaufman <boaz.kaufman97@myhunter.cuny.edu>
2022-04-12 14:36:30 -0400

    working on saving file/researching data encoding

--- app.js
+++ app.js
@@ -2,22 +2,38 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+let lastImage;
 
 // Middleware to parse the req body as text (it's stringified JSON)
 app.use(bodyParser.text());
 
-/* Ignore GET requests!
+
 app.get('/', (req, res) => {
-  res.send(recentimage)
-  console.log('Got a GET request\n');
-})*/
+  res.send(lastImage)
+})*
 
 // When receiving a POST, log its contents to confirm proper receipt
 app.post('/', (req, res) => {
   console.log('Got a POST request ' + Date.now());
-  pic = JSON.parse(req.body);
+  pic = JSON.parse(req.body)._data;
   console.log(pic);
+  lastImage = pic;
   res.append('guess', 'Beep boop this is an image of something!').end();
+
+  // for (key in pic){
+  //   console.log(key);
+  //   console.log(pic[key]);
+  //   console.log();
+  // }
+  // const file = new File(
+	// 	pic,
+	// 	'filename.jpeg',
+	// 	{
+	// 		type: pic.type,
+	// 		lastModified: new Date().getTime()
+	// 	}
+	// )
+
 })
 
 app.listen(port, () => {
